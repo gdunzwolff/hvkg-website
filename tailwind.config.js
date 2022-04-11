91709c2dbfaf0d2a5521bc8d07d6b8de52833256
gdunzwolff <guenter.dunzwolff@gmail.com>
2022-04-11 16:01:03 +0200

    header und footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js,njk}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        kleistBlauGreen: 'hsl(195, 100%, 29%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        kleistBlauGrayscaleVersion: 'hsl(0, 0%, 26%)',
+        kleistBlauComplementRotbraun: 'hsl(15, 100%, 29%)',
+      },
+      fontFamily: {
+        gara: ['Palatino'],
+        garaitalic: ['Palatino-Italic']
+      },
+    },
   },
   plugins: [],
 }
